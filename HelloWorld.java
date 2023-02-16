74d946865c04db33bd6dc6a12f130fb75ee422ff
Sivalingala Harshitha <85675664+HarshithaSivalingala@users.noreply.github.com>
2023-02-16 18:31:48 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("You got hands dirty with jenkins!!"); 
     }
 }
